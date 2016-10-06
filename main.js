b542d6e487003d6a860a071ea002a5343e87c373
Michael Lowe <mikkeyfluttie@gmail.com>
2016-10-06 23:59:14 +1300

    Make so numbers can print on screen

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-var toDisplay = 0;
+var toDisplay = "";
+var currentNum = "";
+var nextNum = "";
+var currentOperation = "";
+var displayLimit = 6;
 
 
 $(document).ready(function(){
@@ -15,28 +19,38 @@ function calculate(){
 
 }
 
-function buttonPress(button){
-  console.log()
+function buttonClick(){
+  console.log("button clicked")
+  console.log("it was this button: "+this.id);
+  return this.id;
 }
 
 
-function setButtonListeners(){
-  // var buttonVals = ['0','1','2','3','4','5','6','7','8','9','add','subtract','multiply','divide','decimal','clear'];
-  document.getElementById('1').addEventListener("click",function(){console.log("1 button pushed");});
-  document.getElementById('2').addEventListener("click",function(){console.log("2 button pushed");});
-  document.getElementById('3').addEventListener("click",function(){console.log("3 button pushed");});
-  document.getElementById('4').addEventListener("click",function(){console.log("4 button pushed");});
-  document.getElementById('5').addEventListener("click",function(){console.log("5 button pushed");});
-  document.getElementById('6').addEventListener("click",function(){console.log("6 button pushed");});
-  document.getElementById('7').addEventListener("click",function(){console.log("7 button pushed");});
-  document.getElementById('8').addEventListener("click",function(){console.log("8 button pushed");});
-  document.getElementById('9').addEventListener("click",function(){console.log("9 button pushed");});
-  document.getElementById('0').addEventListener("click",function(){console.log("0 button pushed");});
-  document.getElementById('add').addEventListener("click",function(){console.log("add button pushed");});
-  document.getElementById('subtract').addEventListener("click",function(){console.log("subtract button pushed");});
-  document.getElementById('divide').addEventListener("click",function(){console.log("divide button pushed");});
-  document.getElementById('equals').addEventListener("click",function(){console.log("equals button pushed");});
-  document.getElementById('decimal').addEventListener("click",function(){console.log("decimal button pushed");});
-  document.getElementById('clear').addEventListener("click",function(){console.log("clear button pushed");});
+function setButtonListeners(){ // Think about this approach when refactoring
+  var buttonVals = ['0','1','2','3','4','5','6','7','8','9','add','subtract','multiply','divide','decimal','clear','equals'];
+  for (var i = 0; i < buttonVals.length; i++){
+    (function () {
+      var currentButton = buttonVals[i];
+      document.getElementById(currentButton).addEventListener("click", function(){
+        console.log("ID: "+this.id);
+        var current = this.id;
+        if (isNaN(current)){ // if its not a number, e.g. multiply, add
+          //add switch statements
+          addition();
+        }
+        else {
+          if (currentNum.length < displayLimit){
+            currentNum =  currentNum + current;
+            console.log("currentNum:"+currentNum);
+            display(currentNum);
+          }
+        }
+      });
+    }()); //immediate invocation to make the function available outside of loop scope
+  }
+  // document.getElementById('1').addEventListener("click",function(){console.log("1 button pushed");});
+}
 
+function addition(){
+  console.log("Addition function exec.");
 }
